368e2cae5460e157f506f1c4fab1483e3d96bff4
duff92 <joakimlarsson92@gmail.com>
2016-09-21 16:37:10 +0200

    Smooth movement of dropped walls

--- Assets/Photon Unity Networking/Demos/MarcoPolo-Tutorial/NetworkCharacter.cs
+++ Assets/Photon Unity Networking/Demos/MarcoPolo-Tutorial/NetworkCharacter.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class NetworkCharacter : Photon.MonoBehaviour
 {
-    private Vector3 correctPlayerPos = Vector3.zero; // We lerp towards this
-    private Quaternion correctPlayerRot = Quaternion.identity; // We lerp towards this
+    private Vector3 correctPos = Vector3.zero; // We lerp towards this
+    private Quaternion correctRot = Quaternion.identity; // We lerp towards this
     // Update is called once per frame
     void Update()
     {
         if (!photonView.isMine)
         {
-            transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
-            transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+            transform.position = Vector3.Lerp(transform.position, this.correctPos, 0.3f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, this.correctRot, 0.3f);
         }
     }
 
@@ -21,18 +21,14 @@ public class NetworkCharacter : Photon.MonoBehaviour
             // We own this player: send the others our data
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
-
-            myThirdPersonController myC = GetComponent<myThirdPersonController>();
-            stream.SendNext((int)myC._characterState);
+            
         }
         else
         {
             // Network player, receive data
-            this.correctPlayerPos = (Vector3)stream.ReceiveNext();
-            this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
-
-            myThirdPersonController myC = GetComponent<myThirdPersonController>();
-            myC._characterState = (CharacterState)stream.ReceiveNext();
+            this.correctPos = (Vector3)stream.ReceiveNext();
+            this.correctRot = (Quaternion)stream.ReceiveNext();
+            
         }
     }
 }
