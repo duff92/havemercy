910050cf9979029e4dc3fff652018b14168e27da
Robin Tillman <robin.tillman92@gmail.com>
2016-09-21 15:49:37 +0200

    VR fixes

---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : Photon.MonoBehaviour {
+
+    public Camera vrCamera;
+
+	// Use this for initialization
+	void Start () {
+	    if(photonView.isMine)
+        {
+            vrCamera.enabled = true;
+        }
+	}
+}
