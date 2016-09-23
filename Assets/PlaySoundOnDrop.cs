d396bb9b993535c2378f0141024b98d5987a9e4b
alan93 <abdlwafa@kth.se>
2016-09-23 12:30:26 +0200

    Added working sounds on wall falling + hitting ground

---
+++ Assets/PlaySoundOnDrop.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundOnDrop : MonoBehaviour {
+
+	public AudioSource walldrop;
+	float initYPos = 0;
+	System.DateTime timerInit;
+	System.DateTime timerCurrent;
+
+	// Use this for initialization
+	void Start () {
+		initYPos = transform.position.y;
+		timerInit = System.DateTime.Now;
+
+		walldrop = GetComponent<AudioSource> ();
+		walldrop.loop = false;
+		walldrop.volume = 0.15f;
+		walldrop.spatialBlend = 0.8f;
+		walldrop.dopplerLevel = 3.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log ("Y: " + transform.position.y);
+		if (transform.position.y <= 3.0326f) {
+			walldrop.Play();
+			timerCurrent = System.DateTime.Now;
+			Debug.Log (timerCurrent - timerInit);
+		}
+	}
+}
---
+++ Assets/PlaySoundOnFall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundOnFall : MonoBehaviour {
+
+	public AudioSource wallfall;
+	public AudioSource[] soundList;
+	float initYPos = 0;
+
+	// Use this for initialization
+	void Start () {
+		initYPos = transform.position.y;
+
+		soundList = GetComponents<AudioSource> ();
+		wallfall = soundList [1];
+		wallfall.loop = false;
+		wallfall.pitch = 0.5f;
+		wallfall.volume = 0.1f;
+		wallfall.spatialBlend = 0.8f;
+		wallfall.dopplerLevel = 3.5f;
+		wallfall.Play ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log ("Y: " + transform.position.y);
+		if (transform.position.y <= 3.0326f) {
+			wallfall.Stop();
+		}
+	}
+}
