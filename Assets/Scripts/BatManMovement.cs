e7e069992240368e4caf3ef39a8bd199eb099851
YInglai Jo <joyinglai@qq.com>
2016-09-23 09:53:46 +0200

    add material and texture for gear UI

---
+++ Assets/Scripts/BatManMovement.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+using VRStandardAssets.Common;
+using VRStandardAssets.Utils;
+
+public class BatManMovement : MonoBehaviour {
+
+
+	public float m_Damping = 0.2f;
+	public float m_MaxYRotation = 50f;
+	public float m_MinYRotation = -50f;
+	public VRInput m_VRInput;
+	public float m_Speed = 100f;   		// the forward movement speed of batman
+	public GameObject m_BatMan;
+
+
+
+
+	private const float k_ExpDampCoef = -50f;
+
+	// get the rotation angle, and let batman rotate
+	private void Update()
+	{
+		// if use vr, get the vr touchpad working 
+		if (VRSettings.enabled) {
+			var eulerRotation = transform.rotation.eulerAngles;
+
+			eulerRotation.x = 0;
+			eulerRotation.z = 0;
+			eulerRotation.y = InputTracking.GetLocalRotation (VRNode.Head).eulerAngles.y;
+
+			if (eulerRotation.y < 270)
+				eulerRotation.y += 360;
+
+			eulerRotation.y = Mathf.Clamp (eulerRotation.y, 360 + m_MinYRotation, 360 + m_MaxYRotation);
+
+			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (eulerRotation), 
+				m_Damping * (1 - Mathf.Exp (k_ExpDampCoef * Time.deltaTime)));
+		} 
+		// if not, use arrows to test
+		else 
+		{
+			if (Input.GetKey(KeyCode.RightArrow)){
+				transform.position += Vector3.right * 3 * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.LeftArrow)){
+				transform.position += Vector3.left* 3 * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.UpArrow)){
+				transform.position += Vector3.forward * 3 * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.DownArrow)){
+				transform.position += Vector3.back* 3 * Time.deltaTime;
+			}
+		}
+
+	}
+
+
+	// handle the click movement 
+	private void OnEnable()
+	{
+		m_VRInput.OnClick += HandleClick;
+	}
+
+
+	private void OnDisable()
+	{
+		m_VRInput.OnClick -= HandleClick;
+	}
+
+	private void HandleClick()
+	{
+		m_BatMan.transform.Translate(Vector3.forward * Time.deltaTime * m_Speed);
+	}
+
+}
---
+++ Assets/VRUIController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using VRStandardAssets.Common;
+using VRStandardAssets.Utils;
+
+public class VRUIController : MonoBehaviour
+{
+	public UIFader m_IntroUI;
+	public UIFader m_OutroUI;
+	//public UIFader m_PlayerUI;
+	//public Text    m_Timer;
+
+	public IEnumerator ShowIntroUI()
+	{
+		yield return StartCoroutine (m_IntroUI.InteruptAndFadeIn ());
+	}
+
+	public IEnumerator HideIntroUI()
+	{
+		yield return StartCoroutine (m_IntroUI.InteruptAndFadeOut ());
+	}
+
+	public IEnumerator ShowOutroUI()
+	{
+		yield return StartCoroutine (m_OutroUI.InteruptAndFadeIn ());
+	}
+
+	public IEnumerator HideOutroUI()
+	{
+		yield return StartCoroutine (m_OutroUI.InteruptAndFadeOut ());
+	}
+//	public IEnumerator ShowPlayerUI()
+//	{
+//		yield return StartCoroutine (m_PlayerUI.InteruptAndFadeIn ());
+//	}
+//
+//	public IEnumerator HidePlayerUI()
+//	{
+//		yield return StartCoroutine (m_PlayerUI.InteruptAndFadeOut ());
+//	}
+}
