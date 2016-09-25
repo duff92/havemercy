956fc6999d3de0de1bf1ce2917fe3d7f886c1ec2
adhag <adrian.haggvik@gmail.com>
2016-09-25 10:04:03 +0200

    objective spawn a distance from the player

--- Assets/AdrianAssets/ObjectiveHandler.cs
+++ Assets/AdrianAssets/ObjectiveHandler.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class ObjectiveHandler : MonoBehaviour {
 
     public int winNr = 5;
+    public float mindist = 7;
+    private float dist;
+    private Vector2 newpos;
+    private GameObject player;
+    private Vector2 playerpos;
+    
 	// Use this for initialization
 	void Start () {
-	
-	}
+        GameObject.FindWithTag("Player");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,9 +22,15 @@ public class ObjectiveHandler : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Wall")
+        if (other.tag == "Player")
         {
-            Vector2 newpos = Random.insideUnitCircle * 18;
+            playerpos = new Vector2(player.transform.position.x, player.transform.position.z);
+            dist = mindist;
+            while(dist <= mindist)
+            {
+                newpos = Random.insideUnitCircle * 18;
+                dist = Vector2.Distance(newpos, playerpos);
+            }
             transform.position = new Vector3(newpos.x, 1.5f, newpos.y);
             winNr -= 1;
             if(winNr == 0)
