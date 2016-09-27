c2b772516ecd78753afea3760f394115ee1664eb
duff92 <joakimlarsson92@gmail.com>
2016-09-27 16:38:10 +0200

    Ongoing progress with cardboard

--- Assets/Scripts/VRMovement.cs
+++ Assets/Scripts/VRMovement.cs
@@ -3,23 +3,23 @@ using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
 
-public class VRMovement : MonoBehaviour {
+public class VRMovement : MonoBehaviour
+{
 
-	float speed = 5.0f;
-	float rotationSpeed = 250.0f;
+    float speed = 5.0f;
+    float rotationSpeed = 250.0f;
 
-	// get the rotation angle, and let batman rotate
-	private void Update()
-	{
-		float translation = CrossPlatformInputManager.GetAxis ("Vertical") * speed;
-		//float rotation = CrossPlatformInputManager.GetAxis ("Horizontal") * rotationSpeed;
-	
-
-		translation *= Time.deltaTime;
-		//rotation *= Time.deltaTime;
-		transform.Translate (0, 0, translation);
-		//transform.rotation = GvrController.Orientation;
-	}
+    // get the rotation angle, and let batman rotate
+    private void Update()
+    {
+        float translation = CrossPlatformInputManager.GetAxis("Vertical") * speed;
+        //float rotation = CrossPlatformInputManager.GetAxis ("Horizontal") * rotationSpeed;
 
 
+        translation *= Time.deltaTime;
+        //rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.position = new Vector3(transform.position.x, 0.29f, transform.position.z);
+        //transform.rotation = GvrController.Orientation;
+    }
 }
