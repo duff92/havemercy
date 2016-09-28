3e9764180044a19f873cf8fe9324601497ab91ee
Joakim Larsson <joakimlarsson92@gmail.com>
2016-09-28 23:05:44 +0200

    Sort of fixed collisions with walls

--- Assets/Scripts/VRMovement.cs
+++ Assets/Scripts/VRMovement.cs
@@ -9,16 +9,21 @@ public class VRMovement : MonoBehaviour
     float speed = 5.0f;
     float rotationSpeed = 250.0f;
 
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     // get the rotation angle, and let batman rotate
     private void Update()
     {
         float translation = CrossPlatformInputManager.GetAxis("Vertical") * speed;
         //float rotation = CrossPlatformInputManager.GetAxis ("Horizontal") * rotationSpeed;
 
-
         translation *= Time.deltaTime;
         //rotation *= Time.deltaTime;
         transform.Translate(0, 0, translation);
+        //rb.MovePosition(new Vector3(transform.position.x, 1.0f, transform.position.z));
         transform.position = new Vector3(transform.position.x, 1.0f, transform.position.z);
         //transform.rotation = GvrController.Orientation;
     }
